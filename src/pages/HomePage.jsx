f8ab3cb99f800d400cd652a4af6de322925f9713
Markussan456 <209953316+Markussan456@users.noreply.github.com>
2026-03-03 22:51:22 +0200

    Add fetching products, Display them

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
 import styles from "./HomePage.module.css";
 
 function HomePage() {
-  return <div className={`${styles.wrapper} page`}>This is the HomePage</div>;
+  return (
+    <div className={`${styles.wrapper} page`}>
+      <h1>Welcome To the Store</h1>
+    </div>
+  );
 }
 
 export default HomePage;
--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,7 +1,33 @@
 import styles from "./shoppage.module.css";
+import ProductCards from "../components/productCard/card";
+import { useEffect, useState } from "react";
 
 function ShopPage() {
-  return <div className={`${styles.wrapper} page`}>This is the ShopPage</div>;
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data.slice(0, 6));
+      });
+  }, []);
+
+  return (
+    <div className={`${styles.wrapper} page`}>
+      <h1>Welcome To the Shop</h1>
+      <div className={styles.product}>
+        {products.map((product) => (
+          <ProductCards
+            key={product.id}
+            title={product.title}
+            imgurl={product.image}
+            price={product.price}
+            description={product.description}
+          ></ProductCards>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ShopPage;
