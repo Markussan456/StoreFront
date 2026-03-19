1690f7531e117b9472c65fe893dca1e04329eb6b
Markussan456 <209953316+Markussan456@users.noreply.github.com>
2026-03-19 15:23:18 +0200

    Add Cartpage display functionality + item counts on cartpage

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,31 @@
 import styles from "./cartpage.module.css";
 
 function CartPage({ products }) {
+  const counts = {};
+
+  products.forEach((element) => {
+    if (counts[element.title]) {
+      counts[element.title] += 1;
+    } else {
+      counts[element.title] = 1;
+    }
+  });
   return (
     <div className={`${styles.wrapper} page`}>
-      {products.map((product) => {
-        return <p>{product.title}</p>;
-      })}
+      <div className={styles.checkoutCont}>
+        <p>Shopping Cart</p>
+        <div className={styles.itemz}>
+          {Object.entries(counts).map(([title, amount]) => {
+            const currItem = products.filter((prod) => prod.title === title);
+            return (
+              <div className={styles.eachItemCont}>
+                <img src={currItem[0].imgurl} alt="" />
+                <p>{title + " x " + amount}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
